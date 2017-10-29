6848d62caf61d10e07b14b6d259f62ef56dccd21
danillopneto <daynilou@gmail.com>
2017-10-29 12:32:49 -0200

    Alteração de tipos de dados do banco de dados

--- apimybike/api/bike/bike.js
+++ apimybike/api/bike/bike.js
@@ -2,14 +2,14 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const bikeSchema = new mongoose.Schema({
-    Nomelocal: { type: Number, required: true },
-    HorarioFuncInicial: { type: Number, required: true },
-    HorarioFuncFinal: { type: Number, required: true },
-    UrlLojaImagem: { type: Number, required: true },
-    MarcaModelo: { type: Number, required: true },
-    Categoria: { type: Number, required: true },
+    Nomelocal: { type: String, required: true },
+    HorarioFuncInicial: { type: String, required: true },
+    HorarioFuncFinal: { type: String, required: true },
+    UrlLojaImagem: { type: String, required: true },
+    MarcaModelo: { type: String, required: true },
+    Categoria: { type: String, required: true },
     Preco: { type: Number, required: true },
-    UrlBikeImagem: { type: Number, required: true }
+    UrlBikeImagem: { type: String, required: true }
 })
 
 module.exports = restful.model('Bike', bikeSchema)
