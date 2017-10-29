78e7e88df0af4a4901ee488e292c795bc3bc5ca4
danillopneto <daynilou@gmail.com>
2017-10-29 13:41:44 -0200

    Implementação da tela de inclusão de bikes

---
+++ js/minhabike.add.js
@@ -0,0 +1,28 @@
+$( "#addBike" ).submit(function( event ) {
+	event.preventDefault();
+    var $inputs = $('#addBike :input');
+    
+    // not sure if you wanted this, but I thought I'd add it.
+    // get an associative array of just the values.
+    var objeto = {};
+    $inputs.each(function() {
+        objeto[this.id] = $(this).val();
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:4004/api/bikes",
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify(objeto),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){
+            alert('Bike adicionada com sucesso!');
+        },
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+    });
+
+    window.location.reload()
+});
